2fa66b130774e457a35c72b635d01bb5329807cf
easinahmed <easintshirtdesigner@gmail.com>
2025-11-01 18:33:35 +0600

    About text and image added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,13 @@
 import { SlashIcon } from "lucide-react"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "../components/ui/breadcrumb"
+import { about_image } from "../constant/constant"
 const About: React.FC = () => {
   return (
     <section className="pt-20">
       <div className="container">
         <div className="about_container">
           <div>
-
+            {/* Breadcrumb from Shadecn UI */}
 
             <Breadcrumb>
               <BreadcrumbList>
@@ -23,14 +24,20 @@ const About: React.FC = () => {
             </Breadcrumb>
           </div>
 
-          <div className="mt-10.5">
-            <div className="about_text">
+          <div className="mt-10.5 flex items-center justify-between gap-19 mb-35">
+            {/* About Text */}
+            <div className=" max-w-[505px]">
 
-            <h1 className="font-inter text-[54px] font-semibold">Our Story</h1>
-            <p className="font-poppins text-base leading-[26px]">Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. 
-              <br /><br />
-              Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.
-            </p>
+              <h1 className="font-inter text-[54px] mb-10 font-semibold">Our Story</h1>
+              <p className="font-poppins text-base leading-[26px]">Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region.
+                <br /><br />
+                Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.
+              </p>
+            </div>
+
+            {/*About Image  */}
+            <div className="about_image">
+              <img src={about_image} alt="image" />
             </div>
 
           </div>
