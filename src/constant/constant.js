628321407b1963d74c75a005805bded3edf62a14
easinahmed <easintshirtdesigner@gmail.com>
2025-11-01 14:35:31 +0600

    Footer Added

---
+++ src/constant/constant.js
@@ -0,0 +1,13 @@
+// Footer Icons
+import facebook from "../assets/Icon-Facebook.svg"
+import instagram from "../assets/icon-instagram.svg"
+import linkedin from "../assets/Icon-Linkedin.svg"
+import twitter from "../assets/Icon-Twitter.svg"
+
+
+
+export {facebook,
+        instagram,
+        linkedin,
+        twitter
+}
