e298b65d03460088dd98adcbff6e8a6d8bdf55a9
easinahmed <easintshirtdesigner@gmail.com>
2025-11-02 19:56:26 +0600

    Contact Page End

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -27,7 +27,7 @@ const Contact: React.FC = () => {
                     <div className="grid grid-cols-[auto_1fr] gap-7.5 ">
                         <div className="py-10 max-w-[340px] shadow-contact px-9">
                             <div className="flex items-center mb-6 gap-4">
-                                <div className="bg-secondery  rounded-full w-10 text-white h-10 flex items-center justify-center">
+                                <div className="bg-button2  rounded-full w-10 text-white h-10 flex items-center justify-center">
                                     <Icon icon="akar-icons:phone" width="20" height="20" />
                                 </div>
                                 <p className="font-poppins font-medium">Call To US</p>
@@ -39,8 +39,8 @@ const Contact: React.FC = () => {
                             {/* divider */}
                             <div className="w-full h-px bg-button mb-8 "></div>
 
-                             <div className="flex items-center mb-6 gap-4">
-                                <div className="bg-secondery  rounded-full w-10 text-white h-10 flex items-center justify-center">
+                            <div className="flex items-center mb-6 gap-4">
+                                <div className="bg-button2  rounded-full w-10 text-white h-10 flex items-center justify-center">
                                     <Icon icon="material-symbols:mail-outline" width="20" height="20" />
                                 </div>
                                 <p className="font-poppins font-medium">Write To US</p>
@@ -53,9 +53,55 @@ const Contact: React.FC = () => {
 
                         </div>
 
-                        <div className="py-10 px-8">
+                        <div className="py-10 px-8 max-w-[800px] shadow-contact">
+                            <div className="grid grid-cols-12 gap-x-4 gap-y-8 mb-8">
+                                {/* Name */}
+                                <div className="relative col-span-4">
+                                    <input
+                                        type="text"
+                                        required
+                                        className="w-full rounded-sm bg-secondary h-[50px] font-poppins text-[rgba(0,0,0,0.53)] py-3 px-4 pr-6"
+                                        placeholder="Your Name"
+                                    />
+                                    <span className="absolute left-28 top-2/5 -translate-y-1/2 text-button2 opacity-50 ">*</span>
+                                </div>
 
+                                {/* Email */}
+                                <div className="relative col-span-4">
+                                    <input
+                                        type="text"
+                                        required
+                                        className="w-full rounded-sm bg-secondary h-[50px] font-poppins text-[rgba(0,0,0,0.53)] py-3 px-4 pr-6"
+                                        placeholder="Your Email"
+                                    />
+                                   <span className="absolute left-28 top-2/5 -translate-y-1/2 text-button2 opacity-50 ">*</span>
+                                </div>
+
+                                {/* Phone */}
+                                <div className="relative col-span-4">
+                                    <input
+                                        type="text"
+                                        required
+                                        className="w-full rounded-sm bg-secondary h-[50px] font-poppins text-[rgba(0,0,0,0.53)] py-3 px-4 pr-6"
+                                        placeholder="Your Phone"
+                                    />
+                                    <span className="absolute left-28 top-2/5 -translate-y-1/2 text-button2 opacity-50 ">*</span>
+                                </div>
+
+                                {/* Message */}
+                                <textarea
+                                    className="h-[207px] bg-secondary rounded-sm resize-none col-span-12 py-3 px-4 text-[rgba(0,0,0,0.49)] font-poppins"
+                                    placeholder="Your Message"
+                                ></textarea>
+                            </div>
+
+                            <div className="flex items-center justify-end">
+                                <button className="bg-button2 py-4 px-12 rounded-sm font-poppins font-medium text-white">
+                                    Send Message
+                                </button>
+                            </div>
                         </div>
+
 
                     </div>
                 </div>
