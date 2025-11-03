e5414fd0e4b0fa5a160efad7075d48ca5ff1ae34
Alamin-Coding <md.alamin.coding@gmail.com>
2025-11-03 20:08:19 +0600

    api fetch

---
+++ src/components/CommonBreadcrumb.tsx
@@ -0,0 +1,34 @@
+
+import { SlashIcon } from "lucide-react"
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "../components/ui/breadcrumb"
+import { Link } from "react-router"
+
+export function CommonBreadcrumb() {
+    return (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                        <Link to="/">Home</Link>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator>
+                    <SlashIcon />
+                </BreadcrumbSeparator>
+                <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                        <Link to="/shop">Shop</Link>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
