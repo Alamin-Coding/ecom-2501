58ff0f2c91bc642edd76407fc86cd1cb1a87a981
easinahmed <easintshirtdesigner@gmail.com>
2025-11-04 18:56:04 +0600

    Loading component added

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,15 +1,16 @@
 import { useGetProductsQuery } from "../api/productApi"
 import { CommonBreadcrumb } from "../components/CommonBreadcrumb"
 import ProductCard from "../components/ProductCard"
+import { Spinner } from "../components/ui/spinner";
 
 const Shop: React.FC = () => {
     const { isLoading, data } = useGetProductsQuery('');
     
   return (
     <section>
-      <div className="container">
-        <CommonBreadcrumb />
-        {isLoading && <p>Product Loading....</p>}
+      <div className="container min-h-screen">
+        <CommonBreadcrumb className="mt-20 mb-12.5"/>
+        {isLoading && <div className="flex items-center text-center justify-center gap-4 text-3xl max-w-[300px]"><Spinner className="size-8"/> Loading....</div>}
         <div className="grid grid-cols-[260px_1fr] gap-6">
             <div className="menu"></div>
             <div className="items grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6">
